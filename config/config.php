1ed32e5e71259417d2541539e1f045a2ecee9177
BryanMoralesJ <bryan.moralesjaramillo@educa.madrid.org>
2023-11-23 08:49:08 +0100

    Configuración básica de la web

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+//Variables donde guardaremos las rutas de nuestros assets
+define("CSS","../assets/css");
+define("JS","../assets/js");
+define("IMG","../assets/img");
+
+$config = [
+    'url' => 'https://127.0.0.1/',
+    'tituloWeb' => 'Página de tareas',
+    
+    //Configuración bdd
+    'host' => 'localhost',
+    'userbd' => 'root',
+    'passdb' => '',
+    'database' => 'proyectoPruebas',
+    'portdb' => 3306
+];
+
+?>
