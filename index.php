d5488d245e4098a5d4fcf2e745279fc3401dd01a
BryanMoralesJ <bryan.moralesjaramillo@educa.madrid.org>
2023-12-01 20:18:03 +0100

    Agregamos el sistema de lenguajes y realizamos el login de la web

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once("config/config.php");
+require_once("language/lang.php");
 
 ?>
 <!DOCTYPE html>
@@ -8,9 +9,28 @@ require_once("config/config.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="assets/js/functions.js"></script>
     <title><?= $config['tituloWeb']; ?></title>
 </head>
 <body>
-    
+    <main class="main">
+        <header class="cabecera">
+                <img src="assets/img/login.png" alt="Bmsolutions" class="cabecera_logo">
+        </header>
+        <div class="contenedor">
+            <form action="" method="POST">
+                <h1 class="contenedor_sesion">Login</h1>
+                <input type="text" name="username" id="username" placeholder="Write your username here">
+                <input type="password" name="password" id="password" placeholder="Write your password here">
+                <label for="language">Select your language</label>
+                <select name="lang">
+                    <option value="en">English</option>
+                    <option value="es">Español</option>
+                </select> <br/>
+                <input type="submit" value="Submit" name="submit">
+            </form>
+        </div>
+    </main>
 </body>
 </html>
---
+++ language/lang.php
@@ -0,0 +1,31 @@
+<?php
+
+    //Variable idioma donde almacenaremos los idiomas que podrá seleccionar el usuario al entrar a la WEB
+    $idioma = [
+        'es' => 'Español',
+        'en' => 'Inglés',
+    ];
+
+    //Variable lenguajes donde se podrá configurar los textos con los idiomas permitidos.
+    $lenguajes = [
+        'es' => [
+            'username' => 'Nombre de usuario',
+            'email' => 'Correo electrónico',
+            'password' => 'Contraseña',
+            'description' => 'Página de tareas donde podrás almacener tu día a día',
+            'footer' => 'Página creada por Bryan Morales',
+            'firstname' => 'Nombre completo',
+            'lastname1' => 'Primer apellido',
+            'lastname2' => 'Segundo apellido'
+
+        ],
+        'en' => [
+            'username' => 'Username',
+            'email' => 'Email',
+            'password' => 'Password',
+            'description' => '',
+            'footer' => 'Page created for Bryan Morales'
+        ]
+    ];
+
+?>
